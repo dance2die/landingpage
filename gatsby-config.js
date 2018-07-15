5afc8fde776f12fe9dcaf91b2e3332749313c091
Sung Kim <sungmkim80@gmail.com>
2018-07-15 01:49:00 -0400

    installed gatsby google analytics package

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,19 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-106610940-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 }
